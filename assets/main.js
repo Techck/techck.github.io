a0e8c3daebcadcbf8c240dd0453c1625c2aff359
Techck <506255654@qq.com>
2018-01-26 11:55:51 +0800

    modify show titlebar

--- assets/main.js
+++ assets/main.js
@@ -4,6 +4,12 @@ var sidebar = document.getElementById("sidebar_id");
 var content = document.getElementById("content_id");
 //当前模式是PC还是手机模式，取反标记，因为第一次会判断一次
 var isInPC = document.body.clientWidth > 600 ? false : true;
+//第一次显示时调整界面
+resizeChange();
+//监听浏览器宽度的改变
+window.onresize = function(){
+    resizeChange();
+};
 /**
  * 获取数据
  */
@@ -12,30 +18,32 @@ var isInPC = document.body.clientWidth > 600 ? false : true;
         console.log(data);
         var info = data;
         setTitleBar(info);
-        resizeChange();
         setProjectData(info);
     });
 })();
-//监听浏览器宽度的改变
-window.onresize = function(){
-    resizeChange();
-};
-//浏览器尺寸变化的回调
+
+/**
+ * adjust the UI when the window'onresize function is called 
+ */
 function resizeChange(){
     //获取网页可见区域宽度
     var totalWidth = document.body.clientWidth;
     if (totalWidth <= 600 && isInPC) {
         isInPC = false;
         //手机模式
-        document.body.removeChild(sidebar);
-        document.body.appendChild(titlebar);
+        sidebar.display = "none";
+        titlebar.display = "block";
+        // document.body.removeChild(sidebar);
+        // document.body.appendChild(titlebar);
         content.style.marginLeft = "0px";
         content.style.marginTop = "60px";
     } else if (totalWidth > 600 && !isInPC) {
         isInPC = true;
         //电脑模式
-        document.body.appendChild(sidebar);
-        document.body.removeChild(titlebar);
+        sidebar.display = "block";
+        titlebar.display = "none";
+        // document.body.appendChild(sidebar);
+        // document.body.removeChild(titlebar);
         content.style.marginLeft = "300px";
         content.style.marginTop = "0px";
     }
